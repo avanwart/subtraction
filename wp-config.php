94e67c750bc5b70f70c366fa3178c13cd70ae334
andywarthol <andywarthol@gmail.com>
2014-04-11 14:42:24 -0600

    Gitignore wpconfig

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'scredesign');
+define('DB_NAME', 'andywart_subtraction');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'andywart_sc');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Subtract123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
