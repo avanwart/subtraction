72bb8839a6cd936c5f6d09182e4e75553b2f3789
andywarthol <andywarthol@gmail.com>
2014-04-13 12:35:21 -0700

    Contact page polish

--- wp-content/themes/builder/contact.php
+++ wp-content/themes/builder/contact.php
@@ -13,6 +13,8 @@ Template Name: Contact Page
 			<div class="col-lg-5 col-lg-offset-1 mantra">
 					<h1><?php the_title(); ?></h1>
 					<p><?php the_field('subtraction_mantra'); ?></p>
+					<p>If you are interested in connecting with or learning more about Subtraction Capital, use the form or send us an email:
+<a href="mailto:info@subtractioncaptial.com">info@subtractioncapital.com</a>.</p>
 			</div>
 			<div class="col-lg-4 col-lg-offset-1">
 				<?php the_content(); ?>
@@ -21,5 +23,10 @@ Template Name: Contact Page
 		<?php endwhile; endif; ?>
 	</div>
 </section>
+<script>
+	jQuery(document).ready(function($){
+		$('textarea').attr('rows', '5');
+	});
+</script>
 
 <?php get_footer(); ?>
