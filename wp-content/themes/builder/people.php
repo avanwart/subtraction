e30bd36d136fd4db263cd0b489a94dc56620d82e
andywarthol <andywarthol@gmail.com>
2014-04-12 18:42:47 -0700

    Fixing gitignore i think and updating pages

---
+++ wp-content/themes/builder/people.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Template Name: People Page
+*/
+
+?>
+<?php get_header(); ?>
+
+<section id="people">
+	<div class="container">
+		<header>
+			<h1><?php the_title(); ?></h1>
+			<p class="mission"><?php the_field('team_description'); ?></p>
+		</header>
+		<div class="row">
+			<div class="col-lg-8 col-md-12 col-lg-offset-2">
+				<h2 class="header">Partners</h2>
+			</div>
+		</div>
+		<div class="row partners">
+			<div class="col-lg-2">
+				&nbsp;
+			</div>
+			<!-- Start the Loop to query portfolio posts -->
+			<?php
+				global $post;
+				$args = array( 'posts_per_page' => 100, 'category' => '3', 'order' => 'ASC', 'orderby' => 'title' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) :  setup_postdata($post); 
+			?>
+			<article class="col-xs-12 col-lg-4 col-md-6 col-sm-6">
+				<div class="inner partner">
+					<h1><?php the_title(); ?></h1>
+					<h2><?php the_field('job_title'); ?></h2>
+					<?php the_post_thumbnail('medium'); ?>
+					<p><?php the_field('biography'); ?></p>
+					<p><strong>Education:</strong> <?php the_field('education'); ?></p>
+					<p><strong>Prior operational experience:</strong> <?php the_field('prior'); ?></p>
+					<a class="linkedin" href="<?php the_field('linkedin_url'); ?>">linkedin</a>
+				</div>
+			</article>
+
+			<!-- Close the loop -->
+			<?php endforeach; ?>
+      <?php wp_reset_postdata(); ?>
+    </div>
+
+		<div class="row">
+			<div class="col-lg-9 col-lg-offset-1 col-md-12 halfset">
+				<h2 class="header">Team</h2>
+			</div>
+		</div>
+    <div class="row partners">
+    	<div class="col-lg-1 halfset">
+  			&nbsp;
+    	</div>
+			<!-- Start the Loop to query portfolio posts -->
+			<?php
+				global $post;
+				$args = array( 'posts_per_page' => 100, 'category' => '5', 'order' => 'ASC', 'orderby' => 'date' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) :  setup_postdata($post); 
+			?>
+				<article class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+					<div class="inner team">
+						<h3><?php the_title(); ?></h3>
+						<h4><?php the_field('job_title'); ?></h4>
+						<?php the_post_thumbnail('medium'); ?>
+						<p><?php the_field('biography'); ?></p>
+						<a class="linkedin" href="http://<?php the_field('linkedin_url'); ?>">linkedin</a>
+					</div>
+				</article>
+
+			<!-- Close the loop -->
+			<?php endforeach; ?>
+	    <?php wp_reset_postdata(); ?>
+
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/builder/portfolio.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Template Name: Portfolio Page
+*/
+
+?>
+<?php get_header(); ?>
+
+<section id="hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h1><?php the_field('boilerplate'); ?></h1>
+				<blockquote>
+					&ldquo;<?php the_field('quote'); ?>&rdquo;
+					<small><?php the_field('quote_author'); ?></small>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="portfolio">
+	<div class="container">
+		<header>
+			<h1><?php the_title(); ?> <small>(alphabetical)</small></h1>
+		</header>
+		<div class="row">
+			<!-- Start the Loop to query portfolio posts -->
+			<?php
+				global $post;
+				$args = array( 'posts_per_page' => 100, 'category_name' => 'companies', 'order' => 'ASC', 'orderby' => 'title' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) :  setup_postdata($post);
+			?>
+			<?php
+				$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' );
+				$url = $thumb['0'];
+			?>
+				<article class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+					<a href="http://<?php the_field('website'); ?>" data-toggle="tooltip" data-placement="top" title="<?php the_field('blurb'); ?>" target="_blank">
+						<div class="inner" style="background: #fff url(<?=$url?>) center center no-repeat; background-size: 75% auto">
+							<?php $thumb_url; ?>
+							<h1><?php the_title(); ?></h1>
+							<?php /*the_post_thumbnail('medium'); */ ?>
+							<!-- <div class="caption">
+								<p><?php the_field('blurb'); ?></p>
+								<a href="http://<?php the_field('website'); ?>"><?php the_field('website'); ?></a>  
+							</div> -->
+						</div>
+					</a>
+				</article>
+
+			<!-- Close the loop -->
+			<?php endforeach; ?>
+      <?php wp_reset_postdata(); ?>
+
+      <script>
+	      jQuery(document).ready(function($){
+	      	$('article a').tooltip();
+	      });
+      </script>
+
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
