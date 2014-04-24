ee516c706b64672607ad7f0b8b94580178441dd0
andywarthol <andywarthol@gmail.com>
2014-04-24 14:01:09 -0700

    Social media on contact page

--- wp-content/themes/builder/contact.php
+++ wp-content/themes/builder/contact.php
@@ -10,25 +10,24 @@ Template Name: Contact Page
 	<div class="container">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="row">
-			<div class="col-lg-5 col-lg-offset-1 mantra">
-					<header>
-						<h1><?php the_title(); ?></h1>
-					</header>
-					<p><?php the_field('subtraction_mantra'); ?></p>
-					<p>If you are interested in connecting with or learning more about Subtraction Capital, use the form or send us an email:
+			<div class="col-lg-8 col-lg-offset-2 mantra">
+				<header>
+					<h1><?php the_title(); ?></h1>
+				</header>
+				<p><?php the_field('subtraction_mantra'); ?></p>
+				<p>If you are interested in connecting with or learning more about Subtraction Capital, use the form or send us an email:
 <a href="mailto:info@subtractioncaptial.com">info@subtractioncapital.com</a>.</p>
-			</div>
-			<div class="col-lg-4 col-lg-offset-1">
-				<?php the_content(); ?>
+				<div class="social">
+					<ul>
+						<li class="facebook"><a href="https://www.facebook.com/subtractioncapital" target="_blank">Facebook</a></li>
+						<li class="linkedin"><a href="http://www.linkedin.com/company/subtraction-capital" target="_blank">LinkedIn</a></li>
+						<li class="twitter"><a href="https://twitter.com/SubtractionCap" target="_blank">Twitter</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<?php endwhile; endif; ?>
 	</div>
 </section>
-<script>
-	jQuery(document).ready(function($){
-		$('textarea').attr('rows', '5');
-	});
-</script>
 
 <?php get_footer(); ?>
